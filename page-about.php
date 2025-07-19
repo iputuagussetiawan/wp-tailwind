5d64f3ee25556b2e173c09a15402a80282730b51
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-07-19 23:04:54 +0800

    page-about section grow bussiness

---
+++ page-about.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Template Name: Theme - About
+*/
+get_header(); 
+    get_template_part('template-parts/sections/about','banner');
+    get_template_part('template-parts/sections/about','why-us');
+get_footer();
+?>
---
+++ template-parts/sections/about-why-us.php
@@ -0,0 +1,33 @@
+<section class="why-us mt-22">
+    <div class="relative line-border grid grid-cols-1 gap-10 lg:grid-cols-2">
+        <div class="lg:border-r lg:border-gray-950/5 dark:border-white/10">
+            <div class=" max-w-full mx-auto">
+                <img class="w-full cover-full" src="<?php echo get_template_directory_uri(); ?>/public/images/company-team.webp" alt="why us">
+            </div>
+        </div>
+        <div class="lg:border-l lg:border-gray-950/5 dark:border-white/10">
+            
+            <div class="grid grid-cols-1 gap-y-2 px-4 py-2 max-lg:line-b sm:px-2 lg:line-b/half">
+                <p class="text-blue-500 dark:text-blue-600 font-mono text-[0.8125rem]/6 font-medium tracking-widest text-pretty uppercase min-[105rem]:absolute min-[105rem]:top-0 min-[105rem]:-left-10 min-[105rem]:origin-bottom-right min-[105rem]:-translate-x-full min-[105rem]:-translate-y-full min-[105rem]:-rotate-90 min-[105rem]:pr-2">Growing your business</p>
+                <h2 class="heading-two">Growing your business.</h2>
+                <div class="mt-4 max-w-2xl  text-gray-600 prose dark:prose-invert">
+                    <div class="mb-4 text-base">
+                        IT can be a powerful tool for growing your business. However, it is important to develop a clear IT strategy that aligns with your business goals and objectives, and to invest in the right technology solutions that will help you achieve those goals.
+                    </div>
+                    <h3 class="mb-0">
+                        Leading the industry
+                    </h3>
+                        <p>Building innovative system development and setting new quality standards</p>
+                    <h3 class="mb-0">
+                        Adapting quickly
+                    </h3>
+                    <p>Releasing new website/mobile apps and listening to your feedback</p>
+                    <h3 class="mb-0">
+                        Continuous improvement
+                    </h3>
+                    <p>We’ll fix what’s and continually adapt to your Scope</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
